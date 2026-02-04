42ba7d9742a4c7fa80019f82ca5ee153dd593288
PC Kantor <pckantorp3ibkpsdm@.kapuaskab.go.id>
2026-02-04 09:23:04 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\DataController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [DataController::class, 'index'])->name('beranda');
+Route::get('/input-dokumen-dbms', [DataController::class, 'index'])->name('beranda');
 Route::get('/search', [DataController::class, 'search'])->name('search');
 Route::post('/upload-dokumen/{nip}/save', [DataController::class, 'uploadDokumen'])->name('upload.dokumen');
+
+Route::redirect('/', 'input-dokumen-dbms');
